13ceb7b06d82088ea817cf99817504a67a4bd439
Steven Pham <stevenpham408@yahoo.com>
2020-09-13 01:27:37 -0700

    Added methods that check if username or email already exists in DB

--- src/main/java/com/stvi/urlshortener/repository/UserRepository.java
+++ src/main/java/com/stvi/urlshortener/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.stvi.urlshortener.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByUsername(String username);
+    public User findByEmail(String email);
 
 
 }
