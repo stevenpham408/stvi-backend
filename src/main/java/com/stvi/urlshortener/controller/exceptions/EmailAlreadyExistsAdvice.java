731dc4936d9f0e4cbae51ea507cca8f15b8cc66e
Steven Pham <stevenpham408@yahoo.com>
2020-09-13 19:44:56 -0700

    Added advice classes for the exceptions to send proper HTTP status code responses

---
+++ src/main/java/com/stvi/urlshortener/controller/exceptions/EmailAlreadyExistsAdvice.java
@@ -0,0 +1,17 @@
+package com.stvi.urlshortener.controller.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EmailAlreadyExistsAdvice {
+    @ResponseBody
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    String emailAlreadyExistsHandler(EmailAlreadyExistsException ex){
+        return ex.getMessage();
+    }
+}
---
+++ src/main/java/com/stvi/urlshortener/controller/exceptions/UsernameAlreadyExistsAdvice.java
@@ -0,0 +1,18 @@
+package com.stvi.urlshortener.controller.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class UsernameAlreadyExistsAdvice {
+    @ResponseBody
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    String usernameAlreadyExistsHandler(UsernameAlreadyExistsException ex){
+        return ex.getMessage();
+    }
+
+}
