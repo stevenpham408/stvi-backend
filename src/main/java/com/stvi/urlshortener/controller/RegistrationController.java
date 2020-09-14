11bfecfebfd4a2a60f05dbf97860de1969453b27
Steven Pham <stevenpham408@yahoo.com>
2020-09-14 01:08:51 -0700

    Added a dummy route to default page for testing purposes

--- src/main/java/com/stvi/urlshortener/controller/RegistrationController.java
+++ src/main/java/com/stvi/urlshortener/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.stvi.urlshortener.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -13,6 +14,10 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class RegistrationController {
     private UserService userService;
+    @RequestMapping("/")
+    public String text(){
+        return "Hello World";
+    }
     @PostMapping("/registration")
     public User createUserAccount(@RequestBody @Valid User proposedUser){
         return userService.registerNewUser(proposedUser);
