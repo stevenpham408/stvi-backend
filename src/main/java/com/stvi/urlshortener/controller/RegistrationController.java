14b03059ad6f83f1f7ff5dc4a265fbb8e067df0b
Steven Pham <stevenpham408@yahoo.com>
2020-09-14 21:01:46 -0700

    Added route to get User by ID & store the salt&hashed PW in DB

--- src/main/java/com/stvi/urlshortener/controller/RegistrationController.java
+++ src/main/java/com/stvi/urlshortener/controller/RegistrationController.java
@@ -1,25 +1,44 @@
 package com.stvi.urlshortener.controller;
 
+import com.stvi.urlshortener.controller.exceptions.EmailAlreadyExistsException;
+import com.stvi.urlshortener.controller.exceptions.UsernameAlreadyExistsException;
+import com.stvi.urlshortener.dto.UserDto;
 import com.stvi.urlshortener.entity.User;
 import com.stvi.urlshortener.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
 public class RegistrationController {
+    private final PasswordEncoder passwordEncoder;
     private UserService userService;
-    @RequestMapping("/")
+    @GetMapping("/login")
     public String text(){
         return "Hello World";
     }
+
+    @GetMapping("/user/{id}")
+    public User getUserAccount(@PathVariable int id){
+        return userService.getUserById(id);
+    }
+
     @PostMapping("/registration")
-    public User createUserAccount(@RequestBody @Valid User proposedUser){
-        return userService.registerNewUser(proposedUser);
+    public User createUserAccount(@RequestBody @Valid UserDto proposedUser){
+        if(userService.doesUsernameExist(proposedUser.getUsername())){
+            throw new UsernameAlreadyExistsException();
+        }
+
+        if(userService.doesEmailExist(proposedUser.getEmail())){
+            throw new EmailAlreadyExistsException();
+        }
+
+        User newUser = new User();
+        newUser.setUsername(proposedUser.getUsername());
+        newUser.setEmail(proposedUser.getEmail());
+        newUser.setPassword(passwordEncoder.encode(proposedUser.getPassword()));
+        return userService.registerNewUser(newUser);
     }
 }
