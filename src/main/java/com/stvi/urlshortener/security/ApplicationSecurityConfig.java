06810ea38a789a8a31cbd8aa9f0705c2a698158a
Steven Pham <stevenpham408@yahoo.com>
2020-09-14 21:02:23 -0700

    Manually generate CSRF token & salt&hash the PW

--- src/main/java/com/stvi/urlshortener/security/ApplicationSecurityConfig.java
+++ src/main/java/com/stvi/urlshortener/security/ApplicationSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.stvi.urlshortener.security;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,18 +11,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 
+    private final PasswordEncoder passwordEncoder;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                // Generate a CSRF token (for some reason it doesn't do it automatically)
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                // Now we authorize requests based on our rules
                 .authorizeRequests()
-                .antMatchers("/")
-                .permitAll()
+                // Anyone is authorized for a request to "/"
+                .antMatchers("/").permitAll()
+                // Only admin roles can request to POST "/registration"
+                .antMatchers("/registration").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -31,7 +44,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
     protected UserDetailsService userDetailsService(){
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(admin);
