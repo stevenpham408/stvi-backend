dd4ed6bc0e5a30558e08f6cb28a16f5047ec62e2
Steven Pham <stevenpham408@yahoo.com>
2020-09-19 03:04:58 -0700

    Added documentation

--- src/main/java/com/stvi/urlshortener/security/PasswordConfig.java
+++ src/main/java/com/stvi/urlshortener/security/PasswordConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * A password configuration class that returns a BCryptPasswordEncoder to salt+hash passwords
+ */
 @Configuration
 public class PasswordConfig {
     @Bean
