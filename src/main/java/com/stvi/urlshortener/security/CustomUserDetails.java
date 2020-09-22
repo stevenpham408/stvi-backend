8c72a32ad090212ec6fc769b1bd38ebba7b32d26
Steven Pham <stevenpham408@yahoo.com>
2020-09-22 03:05:13 -0700

    Removed boilerplate code using Lombok's @SuperBuilder

--- src/main/java/com/stvi/urlshortener/security/CustomUserDetails.java
+++ src/main/java/com/stvi/urlshortener/security/CustomUserDetails.java
@@ -2,34 +2,20 @@ package com.stvi.urlshortener.security;
 
 import com.stvi.urlshortener.entity.UserAccount;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.Set;
 
 @Getter
+@SuperBuilder
 public class CustomUserDetails extends UserAccount implements UserDetails {
     private final Set<? extends GrantedAuthority> grantedAuthorities;
     private final boolean isAccountNonExpired;
     private final boolean isAccountNonLocked;
     private final boolean isCredentialsNonExpired;
     private final boolean isEnabled;
-
-    @Builder(builderMethodName="buildCustomUserDetails")
-    public CustomUserDetails(int id, String username, String password, String email,
-                             Set<? extends  GrantedAuthority> grantedAuthorities,
-                             boolean isAccountNonExpired, boolean isAccountNonLocked,
-                             boolean isCredentialsNonExpired, boolean isEnabled){
-        super(id, username, password, email);
-        this.grantedAuthorities = grantedAuthorities;
-        this.isAccountNonExpired = isAccountNonExpired;
-        this.isAccountNonLocked = isAccountNonLocked;
-        this.isCredentialsNonExpired = isCredentialsNonExpired;
-        this.isEnabled = isEnabled;
-
-    }
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
