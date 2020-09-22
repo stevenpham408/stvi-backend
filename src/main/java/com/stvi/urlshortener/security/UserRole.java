b093743240d12b0d88ccb07eca62ae72acc40963
Steven Pham <stevenpham408@yahoo.com>
2020-09-22 02:59:02 -0700

    Removed 1 unused import

--- src/main/java/com/stvi/urlshortener/security/UserRole.java
+++ src/main/java/com/stvi/urlshortener/security/UserRole.java
@@ -2,7 +2,6 @@ package com.stvi.urlshortener.security;
 
 import com.google.common.collect.Sets;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
@@ -11,9 +10,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public enum UserRole {
     USER(Sets.newHashSet(UserPermission.URL_READ, UserPermission.URL_WRITE));
-
     private final Set<UserPermission> permissions;
-
     public Set<UserPermission> getPermissions(){
         return permissions;
     }
@@ -26,6 +23,4 @@ public enum UserRole {
         permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
-
-
 }
