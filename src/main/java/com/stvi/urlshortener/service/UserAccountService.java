29bbf75216797e64652b6bf34cd7644c7be1f70f
Steven Pham <stevenpham408@yahoo.com>
2020-09-22 02:41:29 -0700

    Renamed UserService.java -> UserAccountService.java

--- src/main/java/com/stvi/urlshortener/service/UserAccountService.java
+++ src/main/java/com/stvi/urlshortener/service/UserAccountService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserAccountService {
     private final UserAccountRepository userRepo;
 
     /**
